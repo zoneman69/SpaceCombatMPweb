5d2ba062cfb00f70ba5ce55eeb9e92f6755c795b
zoneman69 <zoneman69@users.noreply.github.com>
2026-02-07 16:38:38 -0600

    Use shared JS schema runtime exports

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,2 +1,17 @@
 export * from "./types";
-export * from "./state/SpaceState";
+export {
+  BaseSchema,
+  LobbyPlayerSchema,
+  LobbyRoomSchema,
+  ResourceNodeSchema,
+  SpaceState,
+  UnitSchema,
+} from "./state/SpaceState.js";
+export type {
+  BaseSchema,
+  LobbyPlayerSchema,
+  LobbyRoomSchema,
+  ResourceNodeSchema,
+  SpaceState,
+  UnitSchema,
+} from "./state/SpaceState";
